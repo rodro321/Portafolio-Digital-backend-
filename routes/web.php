c7928714170622bb5d7a70cef5d12da2355d5669
Nicolas Jhair Zapata Sagardia <nzapata470@gmail.com>
2026-04-12 23:48:07 -0400

    HU-02: Integracion con backend flores, login GitHub, perfil completo_NICOLAS

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// GitHub OAuth - necesita sesiones (web middleware)
+Route::get('/api/auth/github/redirect', [AuthController::class, 'redirectToGitHub']);
+Route::get('/api/auth/github/callback', [AuthController::class, 'handleGitHubCallback']);
